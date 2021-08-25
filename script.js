609c69ed357ccc551231ca0508e4fefee09a8346
tahsinshaikh <89508355+tahsinshaikh@users.noreply.github.com>
2021-08-25 15:28:49 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
 function forLoopTest () {
    var ran = 0
    var a = new Date()
+
     for (let i = 0; i < 1000000; i++) {
-        ran++
+    	ran++
     }
     var b = new Date()
     console.log('For loop')
-    console.log(`Ran ${ran} times`)
+    console.log(`It has run ${ran} times`)
     console.log(`Total run time in ms: ${b.getTime() - a.getTime()}`)
-    console.log('\n\n')
 }
 
 var elements = []
+
 function createArray () {
     var i = 0
     while (i < 1000000) {
         i++
         elements.push(i)
     }
-    console.log(`Element Array created of length of ${elements.length}\n\n`)
+    console.log(`Element Array of length ${elements.length} created`)
 }
+
 
 function foreachTest () {
     var ran = 0
     var a = new Date()
-    
-    elements.forEach(() => ran++)
-    
+
+    elements.forEach(obj => {
+        ran++
+    });
     var b = new Date()
+
     console.log('foreach')
-    console.log(`Ran ${ran} times`)
+    console.log(`It has run ${ran} times`)
     console.log(`Total run time in ms: ${b.getTime() - a.getTime()}`)
 }
 
 forLoopTest()
+console.log("\n\n\n")
 createArray()
 foreachTest()
